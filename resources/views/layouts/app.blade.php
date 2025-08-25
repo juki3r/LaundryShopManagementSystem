f1bf707c91a2cf66aa5891079c81f3c48e0ff694
juki3r <deepong25@gmail.com>
2025-08-25 21:55:32 +0800

    modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laundry Shop') }}</title>
+        <link rel="shortcut icon" href="{{ asset('logo.png') }}" type="image/x-icon">
 
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
