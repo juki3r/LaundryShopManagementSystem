09bba1f7e8a96255bfceb2c06d708eb27d9d2f7e
juki3r <deepong25@gmail.com>
2025-08-29 08:51:33 +0800

    modified:   database/migrations/0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['Admin', 'Customer'])->nullable()->default('Customer');
             $table->rememberToken();
             $table->timestamps();
         });
