6c90bcebaaabbf6c18c43e71d00e5e61b5ee4745
juki3r <deepong25@gmail.com>
2025-08-29 20:31:18 +0800

    modified:   resources/views/customers/index.blade.php

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight">
-            {{ __('Orders Management') }}
+            {{ __('Customers Management') }}
         </h2>
     </x-slot>
 
@@ -9,11 +9,50 @@
         <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    hello customers
+
+                    <h4 class="mb-4">Users List</h4>
+
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover align-middle">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">Address</th>
+                                     <th scope="col">Phone number</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($customers as $index => $customer)
+                                    <tr>
+                                        <td>{{ $index + 1 }}</td>
+                                        <td>{{ $customer->name }}</td>
+                                        <td>{{ $customer->username }}</td>
+                                        <td>
+                                            <a href="{{ route('customers.show', $customer->id) }}" class="btn btn-sm btn-info">
+                                                View
+                                            </a>
+                                            <a href="{{ route('customers.edit', $customer->id) }}" class="btn btn-sm btn-warning">
+                                                Edit
+                                            </a>
+                                            <form action="{{ route('customers.destroy', $customer->id) }}" method="POST" class="d-inline">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">
+                                                    Delete
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
-
-  
 </x-app-layout>
