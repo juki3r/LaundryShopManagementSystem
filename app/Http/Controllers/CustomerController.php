16e19460718fa46a1bf1d867df5181fdf6575e8a
juki3r <deepong25@gmail.com>
2025-08-29 20:35:31 +0800

    modified:   app/Http/Controllers/CustomerController.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class CustomerController extends Controller
 {
     public function showCustomers()
     {
-        $customers = User::all(); // or User::get();
+        $customers = User::where('role', '!=', 'admin')->paginate(10);
         return view('customers.index', compact('customers'));
     }
 }
