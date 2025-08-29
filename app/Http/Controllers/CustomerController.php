a9992ad8ef8f08c6e29edd55f7c2c9739218fd6f
juki3r <deepong25@gmail.com>
2025-08-29 21:56:44 +0800

    modified:   app/Http/Controllers/CustomerController.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -19,7 +19,7 @@ class CustomerController extends Controller
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'username' => 'required|string|max:255|unique:users,username',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:8',
         ]);
 
         try {
