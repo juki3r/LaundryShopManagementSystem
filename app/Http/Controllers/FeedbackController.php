1e7177bce3a37e843196310a756c0353bfc20b98
juki3r <deepong25@gmail.com>
2025-09-11 21:08:35 +0800

    modified:   app/Http/Controllers/FeedbackController.php

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -16,7 +16,7 @@ class FeedbackController extends Controller
     {
         // Validation
         $validator = Validator::make($request->all(), [
-            'comment' => 'required|string|max:1000',
+            'feedback' => 'required|string|max:1000',
             'rating'  => 'required|integer|min:1|max:5',
         ]);
 
@@ -30,7 +30,7 @@ class FeedbackController extends Controller
         try {
             $feedback = Feedback::create([
                 'user_id' => Auth::id(),           // link to logged-in user
-                'feedback' => $request->comment,
+                'feedback' => $request->feedback,
                 'rating'  => $request->rating,
             ]);
 
