acce18a74f8a7954f0411d29dac445422874ab99
juki3r <deepong25@gmail.com>
2025-09-11 19:54:24 +0800

    new file:   app/Models/Feedback.php 	modified:   app/Models/User.php 	new file:   database/migrations/2025_09_11_115154_create_feedbacks_table.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Order::class);
     }
+
+    public function feedbacks()
+    {
+        return $this->hasMany(Feedback::class);
+    }
 }
