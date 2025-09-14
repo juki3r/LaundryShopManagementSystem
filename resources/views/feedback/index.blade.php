5f4177c242bbc76c8c510aa8e3cbdcc65348e276
juki3r <deepong25@gmail.com>
2025-09-14 19:04:34 +0800

    modified:   resources/views/feedback/index.blade.php

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -6,5 +6,12 @@
     </x-slot>
 
     <div class="py-5">
+        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    hello
+                </div>
+            </div>
+        </div>
     </div>
 </x-app-layout>
