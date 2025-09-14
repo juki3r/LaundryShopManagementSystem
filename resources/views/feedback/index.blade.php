6171bb3fa0e6648c4c8939d4e56b611e25f14f97
juki3r <deepong25@gmail.com>
2025-09-14 19:08:51 +0800

    modified:   resources/views/feedback/index.blade.php

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -6,27 +6,29 @@
     </x-slot>
 
     <div class="py-5">
-        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
                     @if($feedbacks->count() > 0)
                         <div class="space-y-6">
                             @foreach($feedbacks as $feedback)
-                                <div class="border rounded-lg p-4 shadow-sm bg-gray-50">
+                                <div class="border rounded-lg p-5 shadow-sm bg-gray-50 hover:shadow-md transition">
                                     <!-- User -->
-                                    <p class="font-semibold text-gray-700">
+                                    <p class="font-semibold text-gray-800">
                                         {{ $feedback->user->name ?? 'Anonymous' }}
                                     </p>
 
-                                    <!-- Rating as stars -->
+                                    <!-- Rating as gold stars -->
                                     <div class="flex items-center space-x-1 my-2">
                                         @for($i = 1; $i <= 5; $i++)
                                             @if($i <= $feedback->rating)
-                                                <svg class="w-5 h-5 text-yellow-400 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                <!-- Filled star -->
+                                                <svg class="w-5 h-5 text-yellow-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                                     <path d="M10 15l-5.878 3.09 1.122-6.545L.487 6.91l6.561-.955L10 0l2.952 5.955 6.561.955-4.757 4.635 1.122 6.545z"/>
                                                 </svg>
                                             @else
+                                                <!-- Empty star -->
                                                 <svg class="w-5 h-5 text-gray-300 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                                     <path d="M10 15l-5.878 3.09 1.122-6.545L.487 6.91l6.561-.955L10 0l2.952 5.955 6.561.955-4.757 4.635 1.122 6.545z"/>
                                                 </svg>
@@ -35,11 +37,11 @@
                                     </div>
 
                                     <!-- Feedback message -->
-                                    <p class="text-gray-600">{{ $feedback->message }}</p>
+                                    <p class="text-gray-700 leading-relaxed">{{ $feedback->message }}</p>
 
                                     <!-- Date -->
                                     <p class="text-sm text-gray-400 mt-2">
-                                        {{ $feedback->created_at->format('M d, Y h:i A') }}
+                                        {{ $feedback->created_at->format('F d, Y · h:i A') }}
                                     </p>
                                 </div>
                             @endforeach
