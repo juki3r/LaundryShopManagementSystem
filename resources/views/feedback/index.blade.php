0dc59bfdd1c9f1b153301a0ebd82cb0543301fb7
juki3r <deepong25@gmail.com>
2025-09-14 19:13:30 +0800

    modified:   resources/views/feedback/index.blade.php

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -14,30 +14,42 @@
                         <div class="space-y-6">
                             @foreach($feedbacks as $feedback)
                                 <div class="border rounded-lg p-5 shadow-sm bg-gray-50 hover:shadow-md transition">
-                                    <!-- User -->
-                                    <p class="font-semibold text-gray-800">
-                                        {{ $feedback->user->name ?? 'Anonymous' }}
-                                    </p>
+                                    <div class="flex items-center space-x-3">
+                                        <!-- Avatar with first letter -->
+                                        <div class="w-10 h-10 flex items-center justify-center rounded-full bg-yellow-500 text-white font-bold">
+                                            {{ strtoupper(substr($feedback->user->name ?? 'A', 0, 1)) }}
+                                        </div>
 
-                                    <!-- Rating as gold stars -->
-                                    <div class="flex items-center space-x-1 my-2">
-                                        @for($i = 1; $i <= 5; $i++)
-                                            @if($i <= $feedback->rating)
-                                                <!-- Filled star -->
-                                                <svg class="w-5 h-5 text-warning fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                    <path d="M10 15l-5.878 3.09 1.122-6.545L.487 6.91l6.561-.955L10 0l2.952 5.955 6.561.955-4.757 4.635 1.122 6.545z"/>
-                                                </svg>
-                                            @else
-                                                <!-- Empty star -->
-                                                <svg class="w-5 h-5 text-gray-300 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                    <path d="M10 15l-5.878 3.09 1.122-6.545L.487 6.91l6.561-.955L10 0l2.952 5.955 6.561.955-4.757 4.635 1.122 6.545z"/>
-                                                </svg>
-                                            @endif
-                                        @endfor
+                                        <div class="flex-1">
+                                            <!-- User + Rating -->
+                                            <div class="flex items-center justify-between">
+                                                <p class="font-semibold text-gray-800">
+                                                    {{ $feedback->user->name ?? 'Anonymous' }}
+                                                </p>
+
+                                                <div class="flex items-center space-x-1">
+                                                    @for($i = 1; $i <= 5; $i++)
+                                                        @if($i <= $feedback->rating)
+                                                            <!-- Filled star -->
+                                                            <svg class="w-5 h-5 text-yellow-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                                <path d="M10 15l-5.878 3.09 1.122-6.545L.487 6.91l6.561-.955L10 0l2.952 5.955 6.561.955-4.757 4.635 1.122 6.545z"/>
+                                                            </svg>
+                                                        @else
+                                                            <!-- Empty star -->
+                                                            <svg class="w-5 h-5 text-gray-300 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                                <path d="M10 15l-5.878 3.09 1.122-6.545L.487 6.91l6.561-.955L10 0l2.952 5.955 6.561.955-4.757 4.635 1.122 6.545z"/>
+                                                            </svg>
+                                                        @endif
+                                                    @endfor
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
 
                                     <!-- Feedback message -->
-                                    <p class="text-gray-700 leading-relaxed">{{ $feedback->message }}</p>
+                                    <blockquote class="mt-3 border-l-4 border-yellow-400 pl-3 text-gray-700 italic leading-relaxed">
+                                        "{{ $feedback->comment }}"
+                                    </blockquote>
 
                                     <!-- Date -->
                                     <p class="text-sm text-gray-400 mt-2">
