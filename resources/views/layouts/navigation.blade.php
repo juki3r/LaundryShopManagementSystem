be9eedb753b80aedb4d6cf20be79215a6cb792f1
juki3r <deepong25@gmail.com>
2025-09-18 11:33:57 +0800

    modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,7 +24,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M3 9.75L12 3l9 6.75V20a1 1 0 01-1 1h-5.25a.75.75 0 01-.75-.75V14h-4.5v6.25a.75.75 0 01-.75.75H4a1 1 0 01-1-1V9.75z" />
                 </svg>
-                <span class="mx-1" style="font-size: 14px">{{ __('Dashboard') }}</span>
+                <span class="mx-1" style="font-size: 14px; color:black !important">{{ __('Dashboard') }}</span>
             </div>
         </x-nav-link>
         <!-- Orders Nav Link -->
