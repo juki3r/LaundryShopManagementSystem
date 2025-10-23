1e9e2a24f204f568a4704ed7d7db4476a5cb3a19
juki3r <deepong25@gmail.com>
2025-10-24 07:39:52 +0800

    aa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
     <div class="container-fluid h-100 p-0">
         <div class="row g-0 h-100">
             <!-- Sidebar -->
-            <div class="col-12 col-md-2 col-lg-1  sidebar" style="background-color: rgb(26, 247, 255); ">
+            <div class="col-12 col-md-2 col-lg-1  sidebar" style="background-color: rgb(188, 237, 237); ">
                 @include('layouts.navigation')
             </div>
 
